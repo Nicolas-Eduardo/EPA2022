dcaf5ed98d00767aa0ebedefc32db0d580d453ae
Kevin Melo <101608645+Kevin-Melo05@users.noreply.github.com>
2022-10-04 20:33:36 -0300

    Att 2.2

--- js/selecao.js
+++ js/selecao.js
@@ -10,7 +10,7 @@ per1.addEventListener("click", function(){
     verificador = true;
     select.innerHTML = '<img src="css/image/pteste1.png" alt="Não foi possivel carregar a imagem ;-;" width="100%" height="100%"></img>';
     enemy.innerHTML = '<img src="css/image/pteste2.webp" alt="Não foi possivel carregar a imagem ;-;" width="100%" height="100%"></img>';
-    per1.style.border = '3px dotted chartreuse'
+    per1.style.border = '5px inset red'
     per2.style.border = ''
 })
 
@@ -18,7 +18,7 @@ per2.addEventListener("click", function(){
     verificador = true;
     enemy.innerHTML = '<img src="css/image/pteste1.png" alt="Não foi possivel carregar a imagem ;-;" width="100%" height="100%"></img>';
     select.innerHTML = '<img src="css/image/pteste2.webp" alt="Não foi possivel carregar a imagem ;-;" width="100%" height="100%"></img>';
-    per2.style.border = '3px dotted chartreuse'
+    per2.style.border = '5px outset  red'
     per1.style.border = ''
 })
 
