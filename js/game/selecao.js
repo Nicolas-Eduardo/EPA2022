4d535d16dc0b91a07b9fc8e020cf634c244dfd67
Diogo-Mello <diogomellodacrus1108@gmail.com>
2022-10-05 23:53:12 -0300

    correcao 2.0

--- js/game/selecao.js
+++ js/game/selecao.js
@@ -1,13 +1,10 @@
 const body = document.querySelector('body');
 
-body.style.backgroundColor = 'Black';
-
 // if (data.cenario == "cenario1") {
 //     body.style.backgroundImage = "url('css/image/cenario1.gif')";
 // };
-setInterval(function(){
-    setTimeOut(body.style.backgroundImage = "url('css/image/cenario1.gif')", 500);
-},4000);
+
+body.style.backgroundImage = "url('css/image/cenario1.gif')";
 
 
 
