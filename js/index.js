4d5860bbcf196a1eef7e1fcdd6259ba773d850d3
Diogo-Mello <diogomellodacrus1108@gmail.com>
2022-10-20 12:20:04 -0300

    Att 9.9.9

--- js/index.js
+++ js/index.js
@@ -1,13 +1,41 @@
 const index = true;
 const jogo = false;
 
+
+
 const table = document.querySelector('table');
 
-setInterval(function(){
+const audio = document.getElementById('audio');
+const musicButton = document.querySelector('#musicButton');
+
+const audioPlay = new Audio('files/startIndex.wav')
+
+setInterval(function () {
     table.style.outline = "3px dashed #EE0CF2"
 
-    setTimeout(function(){
+    setTimeout(function () {
         table.style.outline = "";
     }, 500)
 }, 1000)
+
+function start(){
+    audioPlay.play()
+    document.querySelector('body').style.backgroundColor = 'black'
+    document.querySelector('body').style.backgroundImage = 'url()';
+    setTimeout(function(){
+        window.location.href = "selecao.html";
+    }, 1000)
+}
+
+function playMusic() {
+    musicButton.setAttribute('src', 'css/image/pause-fill.svg')
+    musicButton.setAttribute('onclick', 'pauseMusic()')
+    audio.play();
+}
+
+function pauseMusic() {
+    musicButton.setAttribute('src', 'css/image/play-fill.svg')
+    musicButton.setAttribute('onclick', 'playMusic()')
+    audio.pause();
+}
 
